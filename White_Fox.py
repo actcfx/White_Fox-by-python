ffc5552f9a4339c1734ddf1712180cf9abad16fd
北極狐 <cheung950624@gmail.com>
2022-07-09 05:52:17 +0800

    Update White_Fox.py

--- White_Fox.py
+++ White_Fox.py
@@ -8,13 +8,33 @@ client = commands.Bot(command_prefix = '/', intents = intents)
 @client.event
 async def on_ready():
     print(f'-> Logged in as {client.user}!')
-    
+
+#成員加入時的事件
 @client.event
 async def on_member_join(member):
-    print(f'-> <@{member.id}> join {member.guild.name} server')
+    match(member.guild.id):
+        case 991002551979737109:
+            channel = client.get_channel(991020606587822181)
+        case 875245594355068958:
+            channel = client.get_channel(994376068293202101)
+    await channel.send(f'歡迎 {member.mention} 加入{member.guild.name}伺服器，玩的開心！')
+    print(f"-> {member} join '{member.guild.name}' server")
 
+#成員離開時的事件
 @client.event
 async def on_member_remove(member):
-    print(f'-> <@{member.id}> leave {member.guild.name} server')
-    
-client.run("OTkxMDQzMjY2Mjk0MjA2NDk0.GWwOKW.UEy-y0CvyeOWaaWKew7TUVIPk23qocH7ChND3c")
+    match(member.guild.id):
+        case 991002551979737109:
+            channel = client.get_channel(991020606587822181)
+        case 875245594355068958:
+            channel = client.get_channel(994376068293202101)
+    await channel.send(f'{member.mention} 離開了{member.guild.name}伺服器，一路好走！')
+    print(f"-> {member} leave '{member.guild.name}' server")
+
+@client.event
+async def on_message(msg):
+    if msg.content[0] == '/':
+        cmd = msg.content.strip('/')
+    cmd = cmd.split(' ')
+
+client.run("OTkxMDQzMjY2Mjk0MjA2NDk0.G_pnhZ.KVXQfButK8cRdDeUe5fjTffo98AE7AIcVXkjpc")
